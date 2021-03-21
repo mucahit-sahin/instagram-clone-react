fa638d8f7da3b1659b302dc489ded28c8e02680a
mucahit-sahin <mucahitsahin786@gmail.com>
2021-03-21 14:27:47 +0300

    AppLinks componenti oluşturuldu

---
+++ src/components/AppLinks/AppLinks.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./AppLinks.scss";
+const AppLinks = () => {
+  return (
+    <div className="appLinks">
+      <span>Uygulamayı indir.</span>
+      <div>
+        <img
+          src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_turkish-tr.png/30b29fd697b2.png"
+          alt="Ios Instagram App"
+        />
+        <img
+          src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_turkish-tr.png/9d46177cf153.png"
+          alt="Android Instagram App"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AppLinks;
